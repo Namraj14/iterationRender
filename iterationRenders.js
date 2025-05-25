4b6df248f447b2d226d17d7dd4c5f92c3eaf0f16
Namraj14 <namrajjoshi14@gmail.com>
2025-05-25 21:46:13 +0530

    Iteration files

---
+++ iterationRenders.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class IterationRenders extends LightningElement {
+    
+    cars = [
+    {id:1, name: "BMW", color:"Blue", Price: "$10,000"},
+    {id:2, name: "Audi", color:"Black", Price: "20,000"},
+    {id:3, name: "Bugatti", color:"Red", Price: "30,000"}
+
+    ]
+}
